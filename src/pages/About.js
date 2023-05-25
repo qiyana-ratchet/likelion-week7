785fa9a398946926fa7d7d50cf02fc10da61901d
qiyana-ratchet <wereter31@konkuk.ac.kr>
2023-05-25 16:50:17 +0900

    Initial Commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+// 필요한 모듈을 불러오세요.
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>소개 페이지로 이동했습니다.</p>
+      <p>Success 페이지로 이동할 수 있는 방법을 찾아 코드를 작성해 주세요!</p>
+
+      {/* 이곳에 코드를 작성해주세요. */}
+      
+    </div>
+  );
+};
+
+export default About;
